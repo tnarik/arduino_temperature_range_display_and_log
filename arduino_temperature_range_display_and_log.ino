58c96248d95da463a3ad9e511d079d82063ede92
tnarik <tnarik@gmail.com>
2014-04-29 07:48:38 +0100

    Converted to use SPI7Segment library.

--- arduino_temperature_range_display_and_log.ino
+++ arduino_temperature_range_display_and_log.ino
@@ -2,6 +2,7 @@
 #include "arduino_temperature_range_display_and_log.h"
 
 #include <SPI.h>
+#include <SPI7Segment.h>
 
 const int sonar_pwm_pin = 3;
 const int temperature_pin = 0;
@@ -14,39 +15,6 @@ int pwm_val () {
   return raw_pwm_value/57.87;
 }
 
-void spi_out_command(byte command, byte data) {
-  digitalWrite(SS, LOW); // low to select
-  SPI.transfer(command);    // command
-  if ( command != 0x76 && command != 0x81 ) {
-    SPI.transfer(data);    // data
-  }
-  digitalWrite(SS, HIGH); // high to deselect
-}
-
-void spi_out_display(byte digit1, byte digit2, byte digit3, byte digit4) {
-  digitalWrite(SS, LOW); // low to select
-  SPI.transfer(digit1);
-  SPI.transfer(digit2);
-  SPI.transfer(digit3);
-  SPI.transfer(digit4);
-  digitalWrite(SS, HIGH); // high to deselect
-}
-
-void spi_out_number(unsigned short num, unsigned short base) {
-  unsigned short digit[4] = { 0, ' ', ' ', ' ' };
-  if ( (base<2) || (base>16) || (num>(base*base*base*base-1)) ) {
-    spi_out_display('x', 'x', 'n', 'o');  // indicates overflow
-  } else {
-    unsigned short place = 0;
-    while ( num && place < 4 ) {
-      if ( num > 0 ) digit[place++] = num%base;
-      num /= base;
-    }
-
-    spi_out_display(digit[3], digit[2], digit[1], digit[0]);
-  }
-}
-
 //The setup function is called once at startup of the sketch
 void setup() {
   analogReference(EXTERNAL);
@@ -56,9 +24,9 @@ void setup() {
   SPI.setDataMode(SPI_MODE0);
   SPI.setClockDivider(SPI_CLOCK_DIV64); //Slow down the master 1/64
 
-  spi_out_command(0x7A, 0x80); // brightness setup (will wear out the flash or eeprom with every write).
+  SPI7Segment.command(0x7A, 0x80); // brightness setup (will wear out the flash or eeprom with every write).
   //Send the reset command to the display - this forces the cursor to return to the beginning of the display
-  spi_out_command('v', ' '); //Reset command
+  SPI7Segment.command('v', ' '); //Reset command
   delay(3000); // wait for initialization
   Serial.begin(9600);
 }
@@ -82,7 +50,7 @@ void loop() {
   Serial.print("centimeters: ");
   Serial.println(cms);
 
-  spi_out_command('v', ' '); //Reset command
-  spi_out_number(int((millivolts - 500 )/10), 10);
+  SPI7Segment.command('v', ' '); //Reset command
+  SPI7Segment.number(int((millivolts - 500 )/10), 10);
   delay(2000);
 }
